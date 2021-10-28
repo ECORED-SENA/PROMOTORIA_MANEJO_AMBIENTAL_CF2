44f67e80bb88ceccceafb7ed4eab2d719eea6828
Andrea Botello <andrea.botellor@misena.edu.co>
2021-10-28 14:46:56 -0500

    updated

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -23,7 +23,7 @@
     .row.justify-content-center(data-aos="fade-down")
       .col-lg-10
         .video
-          iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+          iframe(width="560" height="315" src="https://www.youtube.com/embed/GKYwpgx4zok" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
     
 
     Separador
